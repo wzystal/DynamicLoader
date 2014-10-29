b642bedf7b593db87ba9bd205094980d6c5769ec
Young Z. Wang <wzystal@gmail.com>
2014-10-29 19:18:18 +0800

    代码更新

--- src/com/wzystal/dynamicloader/widget/AppsWidgetProvider.java
+++ src/com/wzystal/dynamicloader/widget/AppsWidgetProvider.java
@@ -1,7 +1,30 @@
 package com.wzystal.dynamicloader.widget;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.wzystal.dynamicloader.R;
 
 public class AppsWidgetProvider extends AppWidgetProvider{
 	
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		for(int widgetId : appWidgetIds){
+			// 获取AppWidget对应的视图
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_apps);
+			
+		}
+	}
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+	}
 }
