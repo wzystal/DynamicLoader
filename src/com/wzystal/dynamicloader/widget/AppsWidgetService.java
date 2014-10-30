f1d60ae9c1f50abe010c0ede5bd6633d0771d823
Young Z. Wang <wzystal@gmail.com>
2014-10-30 19:44:06 +0800

    代码更新

--- src/com/wzystal/dynamicloader/widget/AppsWidgetService.java
+++ src/com/wzystal/dynamicloader/widget/AppsWidgetService.java
@@ -1,11 +1,16 @@
 package com.wzystal.dynamicloader.widget;
 
 import android.content.Intent;
+import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 public class AppsWidgetService extends RemoteViewsService{
 	@Override
 	public RemoteViewsFactory onGetViewFactory(Intent intent) {
 		return null;
+	}
+	
+	class GridViewsFactory implements RemoteViewsFactory{
+		
 	}
 }
